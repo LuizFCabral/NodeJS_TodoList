5ac8a2632552723454ba01128954f0881aef8d49
Luiz Felipe Cabral <luizfelipe.cabralsouza@gmail.com>
2022-03-25 07:18:00 -0300

    adicionando a conexão com o banco
    conexão com o mysql

---
+++ Banco/db.js
@@ -0,0 +1,10 @@
+const conectar = async ()=>{
+    if(global.conexao && global.conexao.state != 'disconected') return global.conexao
+    const mysql=require('mysq12/promise')
+    const con=mysql.createConnection("mysql://root:mariagorete@localhost:3306/test")
+    console.log('Conectou ao banco')
+    global.conexao=con
+    return con
+}
+
+module.exports = {conectar}
